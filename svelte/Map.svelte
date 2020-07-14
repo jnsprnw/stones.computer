64250a8ab688d6fcd9d469344afc140ec6d10b88
z3to <6gqrbj@posteo.org>
2020-07-15 00:26:08 +0200

    Link arcs

--- svelte/Map.svelte
+++ svelte/Map.svelte
@@ -3,6 +3,7 @@
   import { scalePoint } from 'd3-scale';
   import range from 'lodash/range';
   import map from 'lodash/map';
+  import uniqBy from 'lodash/uniqBy';
 
   let width = 0;
   let height = 0;
@@ -24,20 +25,25 @@
     activeEdges = []
   }
 
-  function generateLinks (aspects) {
+  function generateArc (x1, x2, i) {
+    const _y = i ? y / 2 : y + y / 2
+    return `M${x1} ${y} C${x1} ${_y}, ${x2} ${_y}, ${x2} ${y}`
+  }
+
+  function generateLinks (arr) {
     const links = []
-    aspects.forEach(({ x: x1, y: y1, links }) => {
-      links.forEach((link) => {
-        const { x: x2, y: y2 } = aspects.find((node) => node.id === link)
+    arr.forEach(({ x: x1, links: keys, id }) => {
+      keys.forEach((key, i) => {
+        const { x: x2 } = arr.find((node) => node.id === key)
         links.push({
+          source: id,
+          target: key,
           x1,
-          y1,
           x2,
-          y2
+          d: generateArc(x1, x2, i % 2)
         })
       })
     })
-    console.log({ aspects, links })
     return links
   }
 
@@ -57,12 +63,13 @@
   $: aspects = map(nodes, (node) => {
     return {
       ...node,
-      x: x(node.index),
-      y: height / 2
+      x: x(node.index)
     }
   })
 
   $: links = generateLinks(aspects)
+
+  $: y = height / 2
 
   function resize() {
     ({ width, height } = svg.getBoundingClientRect());
@@ -72,7 +79,10 @@
 <svelte:window on:resize='{resize}'/>
 
 <svg bind:this={svg} class="map" class:hasActive={activeEdges.length || activeNodes.length}>
-  {#each aspects as { x, y, title, group, links, id }, i}
+  {#each links as { x1, x2, source, target, d }, i}
+  <path d={d} class:isActive={activeEdges.some((edge) => { return edge[1] === source && edge[0] === target })} />
+  {/each}
+  {#each aspects as { x, title, group, links, id }, i}
   <g on:mouseover={() => handleover([id, ...links], id)} on:mouseleave={handleleave} class:isActive={activeNodes.includes(id)} transform={`rotate(-90, ${x}, ${y})`}>
     <a xlink:href={`#${id}`}>
       <title>Jump to {title}</title>
